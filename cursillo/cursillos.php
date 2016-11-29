ed2e2f313fe8381486e263f53426c04893871fc6
kjplaza32 <kjplaza32@gmail.com>
2016-11-29 16:37:33 -0500

    Create cursillos.php

---
+++ cursillo/cursillos.php
@@ -0,0 +1,39 @@
+<?php
+	error_reporting(E_ALL);
+	ini_set('display_errors', 'On');
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Cursillo</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<!-- Bootstrap -->
+		<link href="../assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+	</head>
+<body>
+	<a href="add.php" target="new">
+		<button type="button" class="btn btn-success">Cursillos</button>
+	</a>
+      
+	<a href="delete.php" target="new">
+		<button type="button" class="btn btn-success">Individuals</button>
+	</a>
+      
+	<a href="list.php" target="new">
+		<button type="button" class="btn btn-success">roles</button>
+	</a>
+      
+	<a href="registered.php" target="new">
+		<button type="button" class="btn btn-success">talks</button>
+	</a>
+	
+	<a href="registrations.php" target="new">
+		<button type="button" class="btn btn-success">parishes</button>
+	</a>
+  
+  <a href="update.php" target="new">
+		<button type="button" class="btn btn-success">parishes</button>
+	</a>
+  
+_END;
