8ad489af8277ec2fe46582bc56ae3cd99ad344a5
kjplaza32 <kjplaza32@gmail.com>
2016-11-29 11:20:43 -0500

    Create home.php

---
+++ home.php
@@ -0,0 +1,30 @@
+<?php
+	error_reporting(E_ALL);
+	ini_set('display_errors', 'On');
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Cursillo</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<!-- Bootstrap -->
+		<link href="../assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+	</head>
+<body>
+      <a href="cursillos.php" target="new">
+								<button type="button" class="btn btn-success">Cursillos</button>
+			</a>
+      
+      <a href="individuals.php" target="new">
+								<button type="button" class="btn btn-success">Individuals</button>
+			</a>
+      
+      <a href="roles.php" target="new">
+								<button type="button" class="btn btn-success">roles</button>
+			</a>
+      
+      <a href="talks.php" target="new">
+								<button type="button" class="btn btn-success">talks</button>
+			</a>
+_END;
