b821a3c82e5c6cd193fa2fb1604ac1555bc8b09f
kjplaza32 <kjplaza32@gmail.com>
2016-11-29 16:43:25 -0500

    Create teams.php

---
+++ team/teams.php
@@ -0,0 +1,23 @@
+<?php
+	error_reporting(E_ALL);
+	ini_set('display_errors', 'On');
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Cursillo</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<!-- Bootstrap -->
+		<link href="../assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+	</head>
+<body>
+	<a href="build.php" target="new">
+		<button type="button" class="btn btn-success">Build Team</button>
+	</a>
+      
+	<a href="member.php" target="new">
+		<button type="button" class="btn btn-success">Team Members</button>
+	</a>
+      
+_END;
